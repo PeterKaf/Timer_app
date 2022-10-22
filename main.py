a4920e18d82b2bf82918b30d8db2a04aa077609f
PiotrKafel <marapablo891@gmail.com>
2022-10-22 23:07:30 +0200

    Input message fix

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import Functions
 Instance_1 = Functions.Timer("Test", "Sample.csv")
 loaded = Instance_1.load_instructions()
 # Instance_1.start_timer(loaded)
-if input("Launch GUI") == "y":
+if input("Press 'y' to launch GUI") == "y":
     Instance_1.gui(loaded)
